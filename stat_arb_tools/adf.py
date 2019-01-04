8e0a9dafb5f124d829590cc58c8bec4b4a7e04fb
adam <ahanna@alumni.mines.edu>
2019-01-03 21:47:59 -0800

    added more params and return vals for adf

--- stat_arb_tools/adf.py
+++ stat_arb_tools/adf.py
@@ -1,10 +1,30 @@
-from statsmodels.tsa.stattools import adfuller
+from enum import Enum
+from math import log
+from typing import List, Tuple, Optional
+from statsmodels.tsa.stattools import adfuller, ResultsStore
 from .beta import calcBetaHat
 from .exceptions import NilListException, MissMatchedLengthException
-from math import log
-from typing import List, Tuple
 
-def adf(l1: List[float]) -> Tuple[float, float]:
+class Regression(Enum):
+    c = "constant"
+    ct = "constant and trend"
+    ctt = "constant, and linear and quadratic trend"
+    nc = "no constant and no trend"
+
+class AutoLag(Enum):
+    aic = "AIC"
+    bic = "BIC"
+    tstat = "t-stat"
+    none = None
+
+def adf(
+    l1: List[float],
+    maxlag: int = None,
+    regression: Regression = Regression.c,
+    autolag: AutoLag = AutoLag.aic,
+    store: bool = False,
+    regresults: bool = False
+) -> Tuple[float, float, int, int, dict, float, Optional[ResultsStore]]:
     """Runs the augmented Dickey-Fuller test.
 
 
@@ -12,7 +32,22 @@ def adf(l1: List[float]) -> Tuple[float, float]:
     ----------
     l1 : list of float
         Time series of ut hat.
-
+    maxlag : int
+        Maximum lag which is included in test, default 12*(nobs/100)^{1/4}
+    regression : Regression enum
+        Constant and trend order to include in regression
+            'c' : constant only (default)
+            'ct' : constant and trend
+            'ctt' : constant, and linear and quadratic trend
+            'nc' : no constant, no trend
+    autolag : Autolag enum
+        if None, then maxlag lags are used
+        if 'AIC' (default) or ‘BIC’, then the number of lags is chosen to minimize the corresponding information criterion
+        't-stat' based choice of maxlag. Starts with maxlag and drops a lag until the t-statistic on the last lag length is significant using a 5%-sized test
+    store : bool
+        If True, then a result instance is returned additionally to the adf 
+    regresults : bool
+        If True, the full regression results are returned. 
 
     Returns
     -------
@@ -29,12 +64,17 @@ def adf(l1: List[float]) -> Tuple[float, float]:
     if len(l1) == 0:
         raise NilListException
 
-    result = adfuller(l1)
+    return adfuller(l1, maxlag = maxlag, regression = regression.name, autolag = autolag.value, store = store, regresults = regresults)
 
-    # statistic, pVal
-    return result[0], result[1];
-
-def isStationary(l1: List[float], l2: List[float]) -> Tuple[float, float]:
+def isStationary(
+    l1: List[float],
+    l2: List[float],
+    maxlag: int = None,
+    regression: Regression = Regression.c,
+    autolag: AutoLag = AutoLag.aic,
+    store: bool = False,
+    regresults: bool = False
+) -> Tuple[float, float, int, int, dict, float, Optional[ResultsStore]]:
     """Runs the augmented Dickey-Fuller test on two time series of asset prices.
 
 
@@ -71,4 +111,4 @@ def isStationary(l1: List[float], l2: List[float]) -> Tuple[float, float]:
     for i in range(len(l1)):
         ut.append(log(l1[i]) - log(l2[i]) - log(betaHat))
 
-    return adf(ut)
+    return adf(ut, maxlag = maxlag, regression = regression, autolag = autolag, store = store, regresults = regresults)
